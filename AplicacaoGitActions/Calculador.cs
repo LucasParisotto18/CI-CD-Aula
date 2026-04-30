e3092059711f6335f1981dfeb96bc3b2cabad54f
Lucas Parisotto <lucas18elias@gmail.com>
2026-04-29 21:13:51 -0300

    PRimeiro Commit

---
+++ AplicacaoGitActions/Calculador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplicacaoGitActions
+{
+    internal class Calculador
+    {
+        public int Somar(int a, int b)
+        {
+            int resultado = a + b;
+            
+            return resultado;
+        }
+
+    }
+}
---
+++ AplicacaoGitActions/SomarTeste.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AplicacaoGitActions
+{
+    internal class SomarTeste
+    {
+        [Fact]
+        public void Somar_DoisNumeros_RetornaSoma()
+        {
+            // Arrange
+            var calculadora = new Calculador();
+            int numero1 = 5;
+            int numero2 = 10;
+            // Act
+            int resultado = calculadora.Somar(numero1, numero2);
+            
+            Assert.Equal(15, resultado);
+        }
+
+    }
+}
